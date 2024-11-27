add5c02c2a8faf9c58690b178bca149442d2246c
oscar <oscar.burman005@gmail.com>
2024-11-27 21:47:07 +0100

    Rendering monsters from api is working

--- js/monster.js
+++ js/monster.js
@@ -1,6 +1,7 @@
 //Js code for monster page
 import { fetchFromApi } from "./common/endpoints.js";
 import { useClickEvent } from "./common/useEvent.js";
+import { renderDataAsUl } from "./common/render.js";
 
 const fetchBtn = document.getElementById("fetchMonsters");
 const monsterContainer = document.getElementById("monsterContainer");
@@ -16,5 +17,10 @@ function init() {
 async function fetchMonsters() {
   console.log("Loading...");
   const response = await fetchFromApi("monsters", "&num=10");
+  if (!response.hasError) {
+    const monsters = response.data.monsters.map((res) => res.name);
+    renderDataAsUl(monsterContainer, "monsterContainer", monsters);
+  } else {
+  }
   console.log("Response: ", response);
 }
