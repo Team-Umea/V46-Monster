6d4be24ace7f51286cd5ece040261d4a0f7a66a4
oscar <oscar.burman005@gmail.com>
2024-11-27 18:05:00 +0100

    Theme toggle js

---
+++ js/common/theme.js
@@ -0,0 +1,34 @@
+const themeToggle = document.getElementById("themeToggle");
+const body = document.body;
+
+window.addEventListener("DOMContentLoaded", () => {
+  init();
+});
+
+function init() {
+  toggleTheme();
+  setEventListener();
+}
+
+function setEventListener() {
+  themeToggle.addEventListener("click", () => {
+    toggleTheme();
+  });
+}
+
+function toggleTheme() {
+  const loadedTheme = null;
+  let theme = "light";
+  if (loadedTheme && loadedTheme === "dark") {
+    theme = "dark";
+  }
+  applyTheme(theme);
+}
+
+function applyTheme(theme) {
+  if (theme === "light") {
+    body.setAttribute("class", "theme-light");
+  } else {
+    body.setAttribute("class", "theme-dark");
+  }
+}
