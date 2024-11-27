058f2ec00c623914ece706e61cb7b55d73192add
oscar <oscar.burman005@gmail.com>
2024-11-27 18:06:26 +0100

    Added save and load in utilities js

--- js/common/utilities.js
+++ js/common/utilities.js
@@ -1,1 +1,13 @@
 //Place the most dynamics functions here that are independet of html elements
+export function save(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function load(key) {
+  try {
+    return JSON.parse(localStorage.getItem(key));
+  } catch (error) {
+    console.log(`${key} does not exists in local storage`);
+    return "";
+  }
+}
