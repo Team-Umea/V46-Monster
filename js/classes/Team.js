d1d4442675d4eef82b8452eee9c0de305bcca211
AndreasZetter <andreas.zetterlund98@gmail.com>
2024-12-02 04:44:57 +0100

    tried to make it work, didnt, its bad

--- js/classes/Team.js
+++ js/classes/Team.js
@@ -1,1 +1,38 @@
 //Class for teams
+export class Team{
+    constructor(teamName){
+        this.name = teamName;
+        this.monsters = [];
+        this.paidFor = false;
+
+    }
+    getPaidFor() {
+        return this.paidFor;
+      }
+    setPaidFor(status) {
+        this.paidFor = status;
+      }
+      getMonsters(){
+        return this.monsters;
+      }
+      setMonsters(monsters) {
+        this.monsters = monsters;
+      }
+      getVisible() {
+        return this.visible;
+      }
+      setVisible(visible) {
+        this.visible = visible;
+      }
+      addMonsterToTeam(monster){
+        this.monsters.push(monster);
+      }
+    static fromJSON(json) {
+        const newTeam = new Team(json.teamName);
+        newTeam.setMonsters(json.monsters);
+        newTeam.setPaidFor(json.paidFor);
+        newTeam.setVisible(json.visible);
+        return newTeam;
+      }
+      
+}
