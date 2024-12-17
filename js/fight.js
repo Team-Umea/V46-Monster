a3038b60bb028f33d6e096b54e9bcb8f401144b7
oscar <oscar.burman005@gmail.com>
2024-12-17 23:21:52 +0100

    Started to set up battle by rendering fighters for each team

--- js/fight.js
+++ js/fight.js
@@ -10,9 +10,9 @@ import { useClickEvent } from "./common/useEvent.js";
 
 const teamsContainerToggle = document.getElementById("teamsContainerToggle");
 const teamsContainerBody = document.getElementById("teamsContainerBody");
-// const teamsStatsContainer = document.getElementById("teamsStatsContainer");
 const teamOneContainer = document.getElementById("teamOne");
 const teamTwoContainer = document.getElementById("teamTwo");
+const battleContainer = document.getElementById("battleContainer");
 
 const teams = load(TEAMS_LSK) || [];
 const selectedTeam = load(SELECTEDFIGHTTEAM_LSK) || null;
@@ -30,6 +30,7 @@ function init() {
 function render() {
   renderTeam(teams[0], teamOneContainer);
   renderTeam(teams[1], teamTwoContainer);
+  renderBattleMonsters();
 }
 
 function toggleTeamsHeadToHead() {
@@ -42,6 +43,57 @@ function toggleTeamsHeadToHead() {
     teamsContainerBody.setAttribute("class", "teamsContainerBody flez");
     setBtnIcon(teamsContainerToggle, "upArrow", "Hide head to head stats");
   }
+}
+
+function toggleTeamMonsters() {
+  const toggleEls = document.querySelectorAll(".teamMonsterToggle");
+
+  const toggles = Array.from(toggleEls);
+
+  toggles.forEach((toggle) => {
+    const index = toggles.indexOf(toggle);
+
+    toggle.addEventListener("click", () => {
+      switch (index) {
+        case 0:
+          const teamOneToggle = teamOneContainer.getElementsByClassName("teamMonsterToggle")[0];
+          const teamOneStats = teamOneContainer.getElementsByClassName("teamStats")[0];
+          const teamOneMonsters = teamOneContainer.getElementsByClassName("teamMonsters")[0];
+
+          const teamOneStatsVisible = teamOneToggle.getAttribute("src").includes("skull");
+
+          if (teamOneStatsVisible) {
+            teamOneStats.setAttribute("class", "teamStats opacity-0 noneVisible");
+            teamOneMonsters.setAttribute("class", "teamMonsters opacity-1 visible");
+            setBtnIcon(teamOneToggle, "teams", "Show team stats");
+          } else {
+            teamOneStats.setAttribute("class", "teamStats opacity-1 visible");
+            teamOneMonsters.setAttribute("class", "teamMonsters opacity-0 noneVisible");
+            setBtnIcon(teamOneToggle, "skull", "Show team monsters");
+          }
+          break;
+        case 1:
+          const teamTwoToggle = teamTwoContainer.getElementsByClassName("teamMonsterToggle")[0];
+          const teamTwoStats = teamTwoContainer.getElementsByClassName("teamStats")[0];
+          const teamTwoMonsters = teamTwoContainer.getElementsByClassName("teamMonsters")[0];
+
+          const teamTwoStatsVisible = teamTwoToggle.getAttribute("src").includes("skull");
+
+          if (teamTwoStatsVisible) {
+            teamTwoStats.setAttribute("class", "teamStats opacity-0 noneVisible");
+            teamTwoMonsters.setAttribute("class", "teamMonsters opacity-1 visible");
+            setBtnIcon(teamTwoToggle, "teams", "Show team stats");
+          } else {
+            teamTwoStats.setAttribute("class", "teamStats opacity-1 visible");
+            teamTwoMonsters.setAttribute("class", "teamMonsters opacity-0 noneVisible");
+            setBtnIcon(teamTwoToggle, "skull", "Show team monsters");
+          }
+          break;
+        default:
+          break;
+      }
+    });
+  });
 }
 
 function renderTeamStats(parent, team) {
@@ -291,252 +343,25 @@ function renderMonstersFought(parent, headerText, record) {
   parent.appendChild(container);
 }
 
-function toggleTeamMonsters() {
-  const toggleEls = document.querySelectorAll(".teamMonsterToggle");
+function renderBattleMonsters() {
+  battleContainer.innerHTML = "";
 
-  const toggles = Array.from(toggleEls);
+  const team1 = teams[0].monsters;
+  const team2 = teams[1].monsters;
 
-  toggles.forEach((toggle) => {
-    const index = toggles.indexOf(toggle);
+  team1.forEach((_, index) => {
+    const team1Monsters = team1[index];
+    const team2Monsters = team2[index];
 
-    toggle.addEventListener("click", () => {
-      switch (index) {
-        case 0:
-          const teamOneToggle = teamOneContainer.getElementsByClassName("teamMonsterToggle")[0];
-          const teamOneStats = teamOneContainer.getElementsByClassName("teamStats")[0];
-          const teamOneMonsters = teamOneContainer.getElementsByClassName("teamMonsters")[0];
+    const fightContainer = document.createElement("div");
 
-          const teamOneStatsVisible = teamOneToggle.getAttribute("src").includes("skull");
+    fightContainer.setAttribute("class", "fightContainer");
 
-          if (teamOneStatsVisible) {
-            teamOneStats.setAttribute("class", "teamStats opacity-0 noneVisible");
-            teamOneMonsters.setAttribute("class", "teamMonsters opacity-1 visible");
-            setBtnIcon(teamOneToggle, "teams", "Show team stats");
-          } else {
-            teamOneStats.setAttribute("class", "teamStats opacity-1 visible");
-            teamOneMonsters.setAttribute("class", "teamMonsters opacity-0 noneVisible");
-            setBtnIcon(teamOneToggle, "skull", "Show team monsters");
-          }
-          break;
-        case 1:
-          const teamTwoToggle = teamTwoContainer.getElementsByClassName("teamMonsterToggle")[0];
-          const teamTwoStats = teamTwoContainer.getElementsByClassName("teamStats")[0];
-          const teamTwoMonsters = teamTwoContainer.getElementsByClassName("teamMonsters")[0];
+    const monsterCardTeam1 = new MonsterFighCard(team1Monsters).card();
+    const monsterCardTeam2 = new MonsterFighCard(team2Monsters).card();
 
-          const teamTwoStatsVisible = teamTwoToggle.getAttribute("src").includes("skull");
+    fightContainer.append(monsterCardTeam1, monsterCardTeam2);
 
-          if (teamTwoStatsVisible) {
-            teamTwoStats.setAttribute("class", "teamStats opacity-0 noneVisible");
-            teamTwoMonsters.setAttribute("class", "teamMonsters opacity-1 visible");
-            setBtnIcon(teamTwoToggle, "teams", "Show team stats");
-          } else {
-            teamTwoStats.setAttribute("class", "teamStats opacity-1 visible");
-            teamTwoMonsters.setAttribute("class", "teamMonsters opacity-0 noneVisible");
-            setBtnIcon(teamTwoToggle, "skull", "Show team monsters");
-          }
-          break;
-        default:
-          break;
-      }
-    });
+    battleContainer.appendChild(fightContainer);
   });
 }
-
-// const fightContainer = document.getElementById("fightContainer");
-// const fightBtn = document.getElementById("fightBtn");
-// const team1Selector = document.getElementById("team1Selector");
-// const team2Selector = document.getElementById("team2Selector");
-// let teams = [];
-
-// async function exampleFight() {
-//   let team1 = await createRandomTeam(3, 4);
-//   let team2 = await createRandomTeam(3, 4);
-//   let param = await buildParam(team1, team2);
-
-//   const data = await serveData("fight", param, fightContainer);
-//   console.log(data);
-
-//   let fightWinner = document.createElement("h2");
-//   fightWinner.setAttribute("class", "winner");
-//   fightWinner.innerText = `${data["teamWon"]} won!`;
-
-//   // Create separate containers for Team 1 and Team 2 details
-//   let team1Details = `<div class="team-details"><h3>Team 1</h3>`;
-//   team1Details += `
-//         <div class="detail"><strong>Battle ID:</strong> ${data.battleID}</div>
-//         <div class="detail"><strong>Team Points:</strong> ${data.team1Points}</div>
-//         <div class="detail"><strong>Remaining HP:</strong></div>
-//     `;
-
-//   // Loop through Remaining HP for Team 1
-//   for (const monster in data.remainingHP.team1) {
-//     const hp = data.remainingHP.team1[monster].hp;
-//     team1Details += `<div class="detail">${monster} has ${hp} HP</div>`;
-//   }
-
-//   // Append Team Stats for Team 1
-//   team1Details += `<div class="detail"><strong>Team Stats:</strong></div>`;
-//   const monsterStats1 = data.teamStats.team1.monsterStats;
-//   for (const monster of monsterStats1) {
-//     team1Details += `<div class="detail">Monster: ${monster.name}, Health: ${monster.health}, Damage: ${monster.damage}</div>`;
-//   }
-//   team1Details += `</div>`; // Close Team 1 details div
-
-//   // Create separate container for Team 2 details
-//   let team2Details = `<div class="team-details"><h3>Team 2</h3>`;
-//   team2Details += `
-//         <div class="detail"><strong>Team Points:</strong> ${data.team2Points}</div>
-//         <div class="detail"><strong>Remaining HP:</strong></div>
-//     `;
-
-//   // Loop through Remaining HP for Team 2
-//   for (const monster in data.remainingHP.team2) {
-//     const hp = data.remainingHP.team2[monster].hp;
-//     team2Details += `<div class="detail">${monster} has ${hp} HP</div>`;
-//   }
-
-//   // Append Team Stats for Team 2
-//   team2Details += `<div class="detail"><strong>Team Stats:</strong></div>`;
-//   const monsterStats2 = data.teamStats.team2.monsterStats;
-//   for (const monster of monsterStats2) {
-//     team2Details += `<div class="detail">Monster: ${monster.name}, Health: ${monster.health}, Damage: ${monster.damage}</div>`;
-//   }
-//   team2Details += `</div>`; // Close Team 2 details div
-
-//   // Update the innerHTML of the fightContainer
-//   fightContainer.innerHTML += team1Details + team2Details;
-
-//   // Append Team Won information
-//   fightContainer.innerHTML += `<div class="detail"><strong>Team Won:</strong> ${data.teamWon}</div>`;
-
-//   // Append the fight winner
-//   fightContainer.append(fightWinner);
-
-//   console.log(fightWinner);
-// }
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   init();
-// });
-
-// function init() {
-//   loadTeams();
-//   addTeamsToSelect();
-//   exampleFight();
-// }
-
-// function loadTeams() {
-//   const loadedTeams = load(TEAMS_LSK);
-//   if (loadedTeams) {
-//     loadedTeams.forEach((loadedTeam) => {
-//       teams.push(Team.fromJSON(loadedTeam));
-//     });
-//   }
-// }
-
-// function addTeamsToSelect() {
-//   team1Selector.setAttribute("class", "teamSelect");
-//   team2Selector.setAttribute("class", "teamSelect");
-
-//   const firstOption1 = document.createElement("option");
-//   const firstOption2 = document.createElement("option");
-
-//   const team1Text = "Choose team 1";
-
-//   firstOption1.innerText = team1Text;
-//   team1Selector.appendChild(firstOption1);
-
-//   const team2Text = "Choose team 2";
-
-//   firstOption2.innerText = team2Text;
-//   team2Selector.appendChild(firstOption2);
-
-//   teams.forEach((team, index) => {
-//     const option = document.createElement("option");
-//     option.setAttribute("value", index);
-//     option.setAttribute("class", "monsterSelectOption");
-//     option.innerText = team.getTeamName();
-//     option.value = team.getTeamName();
-//     team1Selector.appendChild(option);
-//   });
-//   teams.forEach((team, index) => {
-//     const option = document.createElement("option");
-//     option.setAttribute("value", index);
-//     option.setAttribute("class", "monsterSelectOption");
-//     option.innerText = team.getTeamName();
-//     option.value = team.getTeamName();
-//     team2Selector.appendChild(option);
-//   });
-// }
-
-// async function startFight() {
-//   let team1 = teams.find((team) => team.getTeamName() === team1Selector.options[team1Selector.selectedIndex].value);
-//   let team2 = teams.find((team) => team.getTeamName() === team2Selector.options[team2Selector.selectedIndex].value);
-
-//   fightContainer.innerHTML = "";
-//   let param = await buildParam(team1.monsters, team2.monsters);
-//   const data = await serveData("fight", param, fightContainer);
-
-//   let fightWinner = document.createElement("h2");
-//   fightWinner.setAttribute("class", "winner");
-//   fightWinner.innerText = `${data["teamWon"]} won!`;
-//   const details = `
-//     <div class="detail"><strong>Battle ID:</strong> ${data.battleID}</div>
-//     <div class="detail"><strong>Battle Winners:</strong> ${data.battleWinners}</div>
-//     <div class="detail"><strong>Team Points (Team 1):</strong> ${data.team1Points}</div>
-//     <div class="detail"><strong>Team Points (Team 2):</strong> ${data.team2Points}</div>
-//     <div class="detail"><strong>Remaining HP:</strong> ${JSON.stringify(data.remainingHP)}</div>
-//     <div class="detail"><strong>Team Stats:</strong></div>
-//     <div class="detail"><strong>Team 1:</strong> ${JSON.stringify(data.teamStats.team1)}</div>
-//     <div class="detail"><strong>Team 2:</strong> ${JSON.stringify(data.teamStats.team2)}</div>
-//     <div class="detail"><strong>Team Won:</strong> ${data.teamWon}</div>
-//     `;
-
-//   fightContainer.innerHTML += details;
-
-//   fightContainer.append(fightWinner);
-
-//   console.log(fightWinner);
-// }
-
-// fightBtn.addEventListener("click", () => {
-//   startFight();
-// });
-
-// // Create Random Team based on Int (1-10 range for levels)
-// async function createRandomTeam(low, high) {
-//   return await serveData("generateTeam", `&level=${getRandomInt(low, high)}`, fightContainer);
-// }
-
-// //Create parameters to send to API with team1, team2.
-// async function buildParam(team1, team2) {
-//   let team1params = "";
-//   let team2params = "";
-//   let param = "team1=";
-
-//   console.log(team1);
-//   console.log(team2);
-//   for (let i = 0; i < team1.length || i < team2.length; i++) {
-//     if (i < team1.length) {
-//       team1params += team1[i].id; // Concatenate team1's id
-//       team1params += ","; // Add a comma
-//     }
-
-//     if (i < team2.length) {
-//       team2params += Number(team2[i].id); // Concatenate team2's id
-//       team2params += ","; // Add a comma
-//     }
-//   }
-
-//   // Optional: Remove the trailing comma from the parameters
-//   team1params = team1params.slice(0, -1);
-//   team2params = team2params.slice(0, -1);
-
-//   // Return or use the parameters as needed
-//   return param + team1params + "&team2=" + team2params;
-// }
-
-// //Random Int 1-10 range for levels
-// function getRandomInt(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
